1e3024efc323a96fc3196c4ad203e8b119a67f70
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2025-02-18 20:53:26 -0500

    deleting comments

--- src/main/java/com/hexagonal/taks/application/usecases/GetAdditionalTaskInfoUseCaseImpl.java
+++ src/main/java/com/hexagonal/taks/application/usecases/GetAdditionalTaskInfoUseCaseImpl.java
@@ -1,12 +1,8 @@
 package com.hexagonal.taks.application.usecases;
 
 import com.hexagonal.taks.domain.models.AdditionalTaskInfo;
-import com.hexagonal.taks.domain.models.Task;
 import com.hexagonal.taks.domain.ports.in.GetAdditionalTaskInfoUseCase;
 import com.hexagonal.taks.domain.ports.out.ExternalServicePort;
-import com.hexagonal.taks.domain.ports.out.TaskRepositoryPort;
-
-import java.util.Optional;
 
 public class GetAdditionalTaskInfoUseCaseImpl implements GetAdditionalTaskInfoUseCase {
 
